7cafa5ffeac01c1efa5276db750ae054c25114f2
Ethan Dawes <ethand.python@gmail.com>
2025-04-28 14:00:45 -0400

    Enforce IntentionalWeb, publish WatchDog

--- main/js/background.js
+++ main/js/background.js
@@ -1,4 +1,10 @@
-const enforce = new Set(["nfghbmabdoakhobmimnjkamfdnpfammn", "bphljigopmbjfmjbdeedhmeadmefcjbf"]);
+const enforce = new Set([
+    "behjadmmpafnpaeijfoioegdpcbpbhij", // Strict focused youtube (local & web store)
+    "bphljigopmbjfmjbdeedhmeadmefcjbf", // Watchdog (local)
+    "aeieaakfhkjhlaijcboomiokalopdlpc", // Watchdog (web store)
+    "kpomkjpijklmdabpfgphmapbopjockgd", // Intentional Web (web store)
+    "ejnaicioaeopejbifeboomfpcommcnng", // Intentional Web (local, not pinned so can change)
+]);
 
 chrome.management.onDisabled.addListener(info => {
     console.log(info.id);
